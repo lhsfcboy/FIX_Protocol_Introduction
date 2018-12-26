90e849f6708461a9157eedfdba79156ba155f1c8
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2018-12-26 15:49:06 +0900

    Update punctuation_convert.py

--- punctuation_convert.py
+++ punctuation_convert.py
@@ -6,9 +6,9 @@ folder = "FIX_Protocol_Introduction_workspace"
 punctuation_dict = {
     "。" : ".",  # 句号
     "，" : ",",  # 逗号
+    "：" : ":",
     "":"", # 左双引号
     "":"", # 右双引号
-    "":"",
     "":"",
     "":"",
     "":"",
